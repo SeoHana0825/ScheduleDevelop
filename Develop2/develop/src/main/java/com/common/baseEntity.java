67f6d7bb28467bbad042e1b16bac8a5be94e894a
SEO HANA <onnuri1226@gmail.com>
2026-01-23 10:17:01 +0900

    style : 260123 BaseEntity 코드 추가

--- Develop2/develop/src/main/java/com/common/baseEntity.java
+++ Develop2/develop/src/main/java/com/common/baseEntity.java
@@ -1,4 +1,26 @@
 package com.common;
 
-public class baseEntity {
+import jakarta.persistence.*;
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+
+@Getter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public abstract class baseEntity {
+
+    @CreatedDate
+    @Column(updatable = false)
+    @Temporal(TemporalType.TIMESTAMP)
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    @Temporal(TemporalType.TIMESTAMP)
+    private LocalDateTime updatedAt;
+
 }
