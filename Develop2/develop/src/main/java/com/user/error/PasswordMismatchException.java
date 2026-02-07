9e51f92d3601a3c4c6342ca62ccb8db71c404a79
SEO HANA <onnuri1226@gmail.com>
2026-02-07 21:38:05 +0900

    bugfix : ServiceException 연결 버그 수정

--- Develop2/develop/src/main/java/com/user/error/PasswordMismatchException.java
+++ Develop2/develop/src/main/java/com/user/error/PasswordMismatchException.java
@@ -1,11 +1,11 @@
 package com.user.error;
 
-import org.hibernate.service.spi.ServiceException;
+import com.common.error.ServiceException;
 import org.springframework.http.HttpStatus;
 
 public class PasswordMismatchException extends ServiceException {
     public PasswordMismatchException(String message) {
         // HttpStatus.NOT_FOUND 지정
-        super(HttpStatus.BAD_REQUEST, message);
+        super(HttpStatus.BAD_REQUEST,message);
     }
 }
